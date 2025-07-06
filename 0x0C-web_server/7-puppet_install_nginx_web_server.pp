bcd7b0fcb56aa2d0edcfec1ecdfae55618f531a8
eeyueltesfaye <eyueltesfaye6@gmail.com>
2025-07-06 23:37:26 +0300

    working on web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,49 +1,19 @@
-# 7-puppet_install_nginx_web_server.pp
-
-# Ensure Nginx is installed
+# Puppet manifest to install nginx
 package { 'nginx':
   ensure => installed,
 }
 
-# Ensure Nginx service is running and enabled
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
 service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  subscribe  => File['/etc/nginx/sites-available/default'],
-}
-
-# Ensure Hello World! page is in place
-file { '/var/www/html/index.nginx-debian.html':
-  ensure  => file,
-  content => "Hello World!\n",
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  require => Package['nginx'],
-}
-
-# Configure default nginx site
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => @("EOF"),
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}
-  | EOF
-  notify  => Service['nginx'],
-  require => Package['nginx'],
-}
+  ensure  => running,
+  
